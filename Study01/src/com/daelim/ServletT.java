5473793636045bacbc97357275151608a2c2922b
Ktg <gkrgry@gmail.com>
2021-04-18 22:41:15 +0900

    자바파일 없어서 다시 커밋

---
+++ Study01/src/com/daelim/ServletT.java
@@ -0,0 +1,46 @@
+package com.daelim;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletT
+ */
+@WebServlet("/ST")
+public class ServletT extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+			System.out.println("destroy");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet");
+	}
+
+}
---
+++ Study01/src/com/daelim/ServletTest.java
@@ -0,0 +1,56 @@
+package com.daelim;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTest
+ */
+@WebServlet("/mSignUp")
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public ServletTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+System.out.println("doget()");
+		
+		String m_name = request.getParameter("m_name");
+		String m_password = request.getParameter("m_password");
+		
+		System.out.println("m_name " + m_name);
+		System.out.println("m_password " + m_password);
+		
+		Enumeration<String> names = request.getParameterNames();
+		while (names.hasMoreElements()) {
+			String name = (String) names.nextElement();
+			System.out.println("name " + name);
+			
+		}
+		
+		
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		System.out.println("doPost()");
+	
+	}
+	}
+	
+	
